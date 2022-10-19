a7f506559ad4bcd77774da13851c029b84441435
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2022-10-19 11:22:31 +0100

    Footer Component markup and styles- mobile responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './footer.css'
-
+import { BsFacebook } from 'react-icons/bs'
+import { BsTwitter } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
 const Footer = () => {
   return (
-  <section id='footer'>
+    <footer>
+      <a href="#" className='footer__logo'>Femi Emmanuel</a>
 
-  </section>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"> <BsFacebook /></a>
+        <a href="https://instagram.com"> <BsTwitter/></a>
+        <a href="https://twitter.com"> <BsInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small> &copy; Femi Emmanuel. All rights reserved. </small>
+      </div>
+    </footer>
   )
 }
 
